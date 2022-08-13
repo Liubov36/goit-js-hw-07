c3fcea90cd0e742d53477cc88ff8057752a62b8f
Liubov <98466098+Liubov36@users.noreply.github.com>
2022-08-13 17:22:31 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallaryEl = document.querySelector('.gallery');
+function createListGalleryItems(items) {
+    
+    return items.map(({preview, original, description}) => `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+}
+
+const listGallaryItems = createListGalleryItems(galleryItems);
+gallaryEl.insertAdjacentHTML("beforeend", listGallaryItems);
+
+gallaryEl.addEventListener('click', selectImgGallery);
+
+function selectImgGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+      
+  
+  const instance = basicLightbox.create(`
+    <div>
+    <img src="${event.target.dataset.source}"></div>
+`,{
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+    );
+    function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }      
+  }
+  instance.show();    
+}
+
 console.log(galleryItems);
+
+
+// ================basiclightbox======================
+const modalTemplate = () =>  
+    `<div class='modal'>
+        <p>I'm a modal created from a DOM element/node.</p>
+        
+    </div>`;
+
+
+// instance.show();
+
+// const instance = basicLightbox.create(modalTemplate());
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const ulEl = document.querySelector('.gallery');
+function createListGalleryItems(items) {
+    return items.map(({ preview, original, description }) => `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('');
+}
+const listGallaryItems = createListGalleryItems(galleryItems);
+ulEl.insertAdjacentHTML("beforeend", listGallaryItems);
+// ulEl.addEventListener('click', onSelectImgGallery);
 console.log(galleryItems);
+
+// var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+let galleryEl = new SimpleLightbox('.gallery a',{
+    
+    captionPosition: `bottom`,
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+// galleryEl.on('show.simplelightbox', function () {   
+// });
+
+// galleryEl.on('error.simplelightbox', function (e) {
+// 	console.log(e); // Some usefull information
+// });
+
+// with jQuery nearly the same
+// let galleryEl = $('.gallery a').simpleLightbox();
+// galleryEl.on('show.simplelightbox', function () {
+// 	// Do something…
+// });
